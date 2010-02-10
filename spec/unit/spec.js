838fba99c7cf44a48f8b938484b9b02f32e1b6d1
Scott Taylor <scott@railsnewbie.com>
2010-02-10 00:30:39 -0500

    instantiate the builder with new

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,46 +1,54 @@
-describe JM
-  describe JM.Builder
+describe "JM"
+  describe "Builder"
+    before_each
+      builder = new JM.Builder;
+    end
+
+    it "should have a builder"
+      (builder instanceof JM.Builder).should.equal(true);
+    end
+
     it 'should self-close a tag'
-      JM.Builder.ul().should.equal("<ul></ul>");
+      builder.ul().should.equal("<ul></ul>");
     end
 
     it "should use an argument given"
-      JM.Builder.ul({foo: "bar"}).should.equal("<ul foo='bar'></ul>")
+      builder.ul({foo: "bar"}).should.equal("<ul foo='bar'></ul>")
     end
 
     it "should use the correct argument"
-      JM.Builder.ul({bar: "quxx"}).should.equal("<ul bar='quxx'></ul>")
+      builder.ul({bar: "quxx"}).should.equal("<ul bar='quxx'></ul>")
     end
 
     it "should use multiple arguments"
-      JM.Builder.ul({foo: "bar", baz: "quxx"}).should.equal("<ul foo='bar' baz='quxx'></ul>");
+      builder.ul({foo: "bar", baz: "quxx"}).should.equal("<ul foo='bar' baz='quxx'></ul>");
     end
 
     it "should self-close a li"
-      JM.Builder.li().should.equal("<li></li>");
+      builder.li().should.equal("<li></li>");
     end
 
     it "should be able to nest"
-      var result = JM.Builder.ul({"foo": "bar"}, [
-        JM.Builder.li()
+      var result = builder.ul({"foo": "bar"}, [
+        builder.li()
       ]);
 
       result.should.equal("<ul foo='bar'><li></li></ul>");
     end
 
     it "should be able to nest multiple elements"
-      var result = JM.Builder.ul({"foo": "bar"}, [
-        JM.Builder.li(),
-        JM.Builder.li()
+      var result = builder.ul({"foo": "bar"}, [
+        builder.li(),
+        builder.li()
       ]);
 
       result.should.equal("<ul foo='bar'><li></li><li></li></ul>");
     end
 
     it "should be able to nest multiple elements when they are different"
-      var result = JM.Builder.ul({"foo": "bar"}, [
-        JM.Builder.li({foo: "bar"}),
-        JM.Builder.li()
+      var result = builder.ul({"foo": "bar"}, [
+        builder.li({foo: "bar"}),
+        builder.li()
       ]);
 
       result.should.equal("<ul foo='bar'><li foo='bar'></li><li></li></ul>");
