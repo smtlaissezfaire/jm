2828fe9ceadd93d6177745073edbeeea325a83f9
Scott Taylor <scott@railsnewbie.com>
2010-04-27 21:36:45 -0400

    use require.paths.unshift in specs

--- spec/node.js
+++ spec/node.js
@@ -1,7 +1,8 @@
-require("./../lib/jm");
 require('./vendor/jspec/lib/jspec');
 require('./vendor/jspec_dot_reporter/jspec_dot_reporter');
 require('./unit/spec.helper');
+require.paths.unshift("./lib");
+require("jm");
 
 JSpec
   .exec('spec/unit/spec.js')
