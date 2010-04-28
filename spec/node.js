1b660e745003766c7b3fffc5c0736b6683d5c3b0
Scott Taylor <scott@railsnewbie.com>
2010-04-27 21:42:53 -0400

    remove empty spec helper

--- spec/node.js
+++ spec/node.js
@@ -1,6 +1,5 @@
 require('./vendor/jspec/lib/jspec');
 require('./vendor/jspec_dot_reporter/jspec_dot_reporter');
-require('./unit/spec.helper');
 require.paths.unshift("./lib");
 JM = require("jm");
 
