5123b02a14e078332b796ec14448ad6001eaee49
Scott Taylor <scott@railsnewbie.com>
2010-04-27 21:17:16 -0400

    update jm spec helper to run under node v0.1.92

--- spec/node.js
+++ spec/node.js
@@ -1,23 +1,9 @@
-process.mixin(require('sys'));
-require.paths.unshift('spec', '/Users/scotttaylor/.rvm/gems/ruby/1.8.6/gems/jspec-3.2.1/lib', 'lib');
-require('jspec');
-require('unit/spec.helper');
-require('jm');
-
-quit = process.exit;
-print = puts;
-
-readFile = function(path) {
-  var result;
-  require('posix')
-    .cat(path, "utf8")
-    .addCallback(function(contents){ result = contents; })
-    .addErrback(function(){ throw new Error("failed to read file `" + path + "'"); })
-    .wait();
-  return result;
-};
+require("./../lib/jm");
+require('./vendor/jspec/lib/jspec');
+require('./vendor/jspec_dot_reporter/jspec_dot_reporter');
+require('./unit/spec.helper');
 
 JSpec
   .exec('spec/unit/spec.js')
-  .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
+  .run({ reporter: JSpecDotReporter, fixturePath: 'spec/fixtures' })
   .report();
